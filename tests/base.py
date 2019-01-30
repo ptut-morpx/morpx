1693fbeb0bb476b9540c0ce35ad7b027ee3344ea
Nathan DECHER <nathan.decher@gmail.com>
2019-01-30 23:01:21 +0100

    added TUI

--- tests/base.py
+++ tests/base.py
@@ -1,74 +1,9 @@
 from engine import Player, Game
-from ansi import cursor, screen, color, write, flush
-
-# drawing functions
-def drawBoard(game):
-	screen.clear();
-	color.fg('white')
-	color.bg('black')
-	write(" | | || | | || | | \n")
-	write("-+-+-||-+-+-||-+-+-\n")
-	write(" | | || | | || | | \n")
-	write("-+-+-||-+-+-||-+-+-\n")
-	write(" | | || | | || | | \n")
-	write("=====##=====##=====\n")
-	write(" | | || | | || | | \n")
-	write("-+-+-||-+-+-||-+-+-\n")
-	write(" | | || | | || | | \n")
-	write("-+-+-||-+-+-||-+-+-\n")
-	write(" | | || | | || | | \n")
-	write("=====##=====##=====\n")
-	write(" | | || | | || | | \n")
-	write("-+-+-||-+-+-||-+-+-\n")
-	write(" | | || | | || | | \n")
-	write("-+-+-||-+-+-||-+-+-\n")
-	write(" | | || | | || | | \n")
-	color.reset()
-	flush()
-
-def drawPiece(game, player, x1, y1, x2, y2):
-	cursor.save()
-	cursor.set(x2*2+x1*7+1, y2*2+y1*6+1)
-	if player==Player.P1:
-		color.fg('blue')
-		write('X')
-	else:
-		color.fg('yellow')
-		write('O')
-	color.reset()
-	cursor.restore()
-	flush()
-
-def drawSubWin(game, player, x, y):
-	print(player.name, 'wins', x, y)
-	cursor.save()
-	if player==Player.P1:
-		color.bg('blue')
-	else:
-		color.bg('yellow')
-	for i in range(3):
-		for j in range(3):
-			cursor.set(i*2+x*y+1, j*2+y*6+1)
-			color.fg('black')
-			p=game.getCell(x, y, i, j)
-			if p==Player.P1:
-				write('X')
-			elif p==Player.P2:
-				write('O')
-			else:
-				write(' ')
-	color.reset()
-	cursor.restore()
-	flush()
-
-def drawWin(game, player):
-	print(player.name, 'wins')
+import tui
 
 # actual code
 game=Game()
-game.addResetTrigger(drawBoard)
-game.addCellTrigger(drawPiece)
-game.addSubWinnerTrigger(drawSubWin)
+tui.install(game)
 
 game.reset()
 game.play(Player.P1, 0, 0, 1, 1)
